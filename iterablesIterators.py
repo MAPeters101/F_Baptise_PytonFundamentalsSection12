57617940a1ee60248748f1ffff0e0284df06891d
Mark Peters <mapeters101@earthlink.net>
2024-10-16 16:29:18 -0400

    Lesson106: Work on interables and iterators up to 4:15.

---
+++ iterablesIterators.py
@@ -0,0 +1,23 @@
+l = [1,2,3]
+
+iterator = iter(l)
+print(type(iterator))
+print(id(iterator))
+print(next(iterator))
+print(next(iterator))
+print(next(iterator))
+#print(next(iterator))
+print('DONE')
+
+iterator = iter(l)
+print(type(iterator))
+print(id(iterator))
+print(next(iterator))
+print(next(iterator))
+print(next(iterator))
+#print(next(iterator))
+print('DONE')
+print('='*30)
+l = [1,2,3,4,5]
+for element in l:
+    print(element)
