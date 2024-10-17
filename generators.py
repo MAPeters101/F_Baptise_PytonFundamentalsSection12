018c50acfa531775fcbfa6097ff48224f456c8c2
Mark Peters <mapeters101@earthlink.net>
2024-10-16 23:42:02 -0400

    Lesson108: Complete work with generators from 6:05.

--- generators.py
+++ generators.py
@@ -38,3 +38,12 @@ squares = (i ** 2 for i in range(5))
 print(4 in squares)
 print(list(squares))
 
+from timeit import timeit
+print(timeit('[i ** 2 for i in range(25_000_000)]', number=1))
+print(timeit('(i ** 2 for i in range(25_000_000))', number=1))
+print(timeit('(i ** 2 for i in range(250_000_000_000))', number=1))
+
+
+
+
+
